72572ce48a8d0eaae3e35099741806c99c551c16
I-graham <54394607+I-graham@users.noreply.github.com>
2019-09-14 13:48:34 -0400

    Update BOB scanner.py

--- BOB scanner.py
+++ BOB scanner.py
@@ -1,6 +1,12 @@
 import re
 import os
 
+def isLink(string):
+    if re.match("LINKS:", string.upper()) or re.match("HTTPS*://", string.upper()):
+        return True
+    else:
+        return False
+
 def isNew(string):
     if re.match(r"ELIGIBLE", string.upper()):
         return True
@@ -18,13 +24,42 @@ fileopen = open(directory +"\\BOB.txt","r+")
 lineList = fileopen.readlines()
 lineList.append('')
 doPrint = False
+linkPassed = True
 
 for line in range(0, len(lineList)-1):
-    if isNew(lineList[line+1]):
-        if isSoph(lineList[line+1]):
-            doPrint=True
-        else:
-            doPrint=False
-            print('\n\n\n\n\n')
+    if line > 0:
+        if lineList[line] in lineList[line-1] and isLink(lineList[line-1]):
+            continue
+
+    if '__________________________________________________________________' in lineList[line]:
+        break
+
+    if isLink(lineList[line]) and ' ' not in lineList[line+1]:
+        lineList[line] += lineList[line+1]
+        lineList[line].replace('\n', '')
+
+    if linkPassed and not isLink(lineList[line]):
+        for i in range(line, len(lineList)-1):
+
+            if isLink(lineList[i]):
+                linkPassed = True
+            else:
+                linkPassed = False
+
+            if linkPassed and not isLink(lineList[i]):
+                break
+            elif isNew(lineList[i]):
+                if isSoph(lineList[i]):
+                    doPrint = True
+                    break
+                else:
+                    doPrint = False
+                    break
+
+    if isLink(lineList[line]):
+        linkPassed = True
+    else:
+        linkPassed = False
+
     if doPrint:
         print(lineList[line])
